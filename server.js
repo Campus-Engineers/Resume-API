1bcfbde5323a3e434f07926f730b5182d0a65447
Peter Nguyen <peternguyenforwork@gmail.com>
2023-12-20 16:22:31 -0800

    update: reset the code to starter code

--- server.js
+++ server.js
@@ -4,14 +4,8 @@ const app = express();
 const path = require("path");
 
 app.get("/", function (req, res) {
-  // __dirname is an absolute path to the directory containing the currently executing file
-  console.log("__dirname = ", __dirname);
-  // send the index.html file to the client when they access the '/' path on our server
-  // path.join() method joins all given path segments together
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.send("Hello Node");
 });
-
-app.get("/api", (req, res) => {});
 
 app.listen(3000, () => {
   console.log("App is listening on Port 3000");
