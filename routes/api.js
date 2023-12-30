24be982a668a81743a5a503fbafb91f177924493
Peter Nguyen <peternguyenforwork@gmail.com>
2023-12-29 19:41:29 -0800

    added the most up-to-date code for session 8

--- routes/api.js
+++ routes/api.js
@@ -1,32 +1,56 @@
-const express = require("express") //import express
-const router = express.Router() //instatiate the router
+const express = require("express"); //import express
+const router = express.Router(); //instatiate the router
 
 var experiences = [];
 
 router.use("/", (req, res, next) => {
-    console.log("API request received");
-    next();
+  console.log("API request received");
+  next();
 });
-  
+
 router.get("/intro", (req, res) => {
-    // send a json response with the status code of 200 to the client
-    // json() method takes a json object as an argument, converts it to a json-like string,
-    // and then sends it to the client
-    res.status(200).json({
-      name: "Vinh Pham",
-      university: "California State University, East Bay",
-      major: "Computer Science",
-    });
+  // send a json response with the status code of 200 to the client
+  // json() method takes a json object as an argument, converts it to a json-like string,
+  // and then sends it to the client
+  res.status(200).json({
+    name: "Vinh Pham",
+    university: "California State University, East Bay",
+    major: "Computer Science",
+  });
 });
-  
+
 router.get("/experience", (req, res) => {
-    res.status(200).json(experiences);
+  res.status(200).json(experiences);
+});
+
+router.put("/:id", (req, res) => {
+  const expIndex = parseInt(req.params.id);
+  if (expIndex >= 0 && expIndex < experiences.length) {
+    // Update the experience at the found index
+    experiences[expIndex] = { ...experiences[expIndex], ...req.body };
+    res.status(200).json({ message: "Experiences updated successfully" });
+  } else {
+    res.status(404).json({ message: "Experiences not found" });
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  const expIndex = parseInt(req.params.id);
+
+  if (expIndex >= 0 && expIndex < experiences.length) {
+    // Delete the experience at the found index
+    // delete one element at the  expIndex
+    experiences.splice(expIndex, 1);
+    res.status(200).json({ message: "Experience deleted successfully" });
+  } else {
+    res.status(404).json({ message: "Experience not found" });
+  }
 });
 
 router.post("/experience", (req, res) => {
-    const newExperience = req.body;
-    experiences.push(newExperience); //say you got a new job
-    res.status(201).json({message: 'New job was added'})
+  const newExperience = req.body;
+  experiences.push(newExperience); //say you got a new job
+  res.status(201).json({ message: "New job was added" });
 });
 
-module.exports = router
+module.exports = router;
